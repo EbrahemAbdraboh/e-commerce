47dcd5c65e8f9f065412e3b44823bb2bcf544cc2
EbrahemAbdraboh <ebrahem.abdraboh@gmail.com>
2020-08-25 01:36:30 +0200

    Updates

---
+++ precache-manifest.0e08218ee97a428a5d130e84c2433e62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2322ea8d9c7cee849f6b42f36b2339",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "0452efca2151108dcb3e",
+    "url": "/e-commerce/static/css/main.9251eba9.chunk.css"
+  },
+  {
+    "revision": "2bab584546f526a63aab",
+    "url": "/e-commerce/static/js/2.202a29c5.chunk.js"
+  },
+  {
+    "revision": "0452efca2151108dcb3e",
+    "url": "/e-commerce/static/js/main.8b504ba0.chunk.js"
+  },
+  {
+    "revision": "290256ac1007569aecdb",
+    "url": "/e-commerce/static/js/runtime~main.8575550b.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/e-commerce/static/media/crown.ed28accb.svg"
+  }
+]);
